6a96390b60fc10efbe76413116ab31bbef60d2d8
jaroenchai tantipong <jaroechai@gmail.com>
2020-06-08 17:56:38 +0700

    init code

---
+++ ImageProcessing/Demo1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Demo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrow = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrow
+            // 
+            this.btnBrow.Location = new System.Drawing.Point(12, 12);
+            this.btnBrow.Name = "btnBrow";
+            this.btnBrow.Size = new System.Drawing.Size(75, 23);
+            this.btnBrow.TabIndex = 0;
+            this.btnBrow.Text = "...";
+            this.btnBrow.UseVisualStyleBackColor = true;
+            this.btnBrow.Click += new System.EventHandler(this.btnBrow_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 415);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 473);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrow;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ImageProcessing/Demo1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo1
+{
+    public partial class Form1 : Form
+    {
+        ConfigurationDetector configurationDetector;
+        YoloConfiguration config;
+        public Form1()
+        {
+            InitializeComponent();
+            configurationDetector = new ConfigurationDetector();
+            config = configurationDetector.Detect();
+        }
+        private IEnumerable<YoloItem> Detect()
+        {
+
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                MemoryStream ms = new MemoryStream();
+
+                pictureBox1.Image.Save(ms, ImageFormat.Png);
+                Image img = pictureBox1.Image;
+                return yoloWrapper.Detect(ms.ToArray());
+
+            }
+        }
+
+        private void btnBrow_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PNG|*.png|JPEG|*.jpg" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    pictureBox1.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            IEnumerable<YoloItem> detectItem = Detect();
+            DrawDetact(detectItem);
+            MessageBox.Show("Done");
+        }
+        private Brush GetBrush(double confidence)
+        {
+            if (confidence > 0.5)
+            {
+                return Brushes.GreenYellow;
+            }
+            else if (confidence > 0.2 && confidence <= 0.5)
+            {
+                return Brushes.Orange;
+            }
+
+            return Brushes.DarkRed;
+        }
+        private void DrawDetact(IEnumerable<YoloItem> detectItem)
+        {
+            if (detectItem != null && detectItem.Count() > 0)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    pictureBox1.Invoke((MethodInvoker)delegate
+                    {
+                        pictureBox1.Image.Save(ms, ImageFormat.Png);
+
+                        Image img = pictureBox1.Image;
+                        using (var font = new Font(FontFamily.GenericSansSerif, 16))
+                        using (var canvas = Graphics.FromImage(img))
+                        {
+                            foreach (var item in detectItem)
+                            {
+                                var x = item.X;
+                                var y = item.Y;
+                                var width = item.Width;
+                                var height = item.Height;
+
+                                var brush = this.GetBrush(item.Confidence);
+                                var penSize = img.Width / 100.0f;
+
+                                using (var pen = new Pen(brush, penSize))
+                                {
+                                    canvas.DrawRectangle(pen, x, y, width, height);
+                                    canvas.FillRectangle(brush, x - (penSize / 2), y - 15, width + penSize, 25);
+                                }
+                            }
+
+                            foreach (var item in detectItem)
+                            {
+                                canvas.DrawString(item.Type, font, Brushes.White, item.X, item.Y - 12);
+                            }
+
+
+                            canvas.Flush();
+                        }
+                        pictureBox1.Image = img;
+                    });
+                }
+
+
+            }
+
+
+        }
+    }
+}
